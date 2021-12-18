41af7af4b66c613e187bfa310049a0fd9df0e935
alexkrushkova <alexkrushkova@gmail.com>
2021-12-18 17:16:51 +0100

    category item link updated

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
 import styled from 'styled-components'
 import {mobile} from '../responsive'
 
@@ -41,13 +42,15 @@ const Button= styled.button`
 const CategoryItem = ({item}) => {
     return (
         <Container>
-            <Image src={item.img}/>
+          <Link to={`/products/${item.cat}`}>
+            <Image src={item.img} />
             <Info>
-                <Title>{item.title}</Title>
-                <Button>SHOP NOW</Button>
+              <Title>{item.title}</Title>
+              <Button>SHOP NOW</Button>
             </Info>
+          </Link>
         </Container>
-    )
+      );
 }
 
 export default CategoryItem
