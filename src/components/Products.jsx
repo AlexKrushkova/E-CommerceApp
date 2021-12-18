f47a0acf5c55db66b4f860f2f71b8b3e33c745cd
alexkrushkova <alexkrushkova@gmail.com>
2021-12-18 18:36:44 +0100

    sorting and fitler updated

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import { popularProducts } from '../data';
 import Product from '../components/Product';
+import { useState, useEffect } from "react";
+
 
 const Container = styled.div`
     padding: 20px;
@@ -9,8 +11,8 @@ const Container = styled.div`
     flex-wrap: wrap;
     justify-content: space-between;
 `;
-const Products = () => {
-
+const Products = ({cat, filters, sort}) => {
+    console.log(cat,filters, sort);
   return (
     <Container>
       {popularProducts.map(item => (
