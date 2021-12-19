4c40c6c04ed33fb03725ccdbb9023ed1629b0786
alexkrushkova <alexkrushkova@gmail.com>
2021-12-19 17:02:13 +0100

    requestMethods.js created and updated

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,6 +1,8 @@
 import { FavoriteBorderOutlined, SearchOutlined, ShoppingCartOutlined } from '@material-ui/icons';
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from "react-router-dom";
+
 
 const Info = styled.div`
 opacity: 0;
@@ -78,7 +80,9 @@ const Product = ({ item }) => {
                     <ShoppingCartOutlined />
                 </Icon>
                 <Icon>
+                    <Link to={`/product/${item._id}`}>
                     <SearchOutlined />
+                    </Link>
                 </Icon>
                 <Icon>
                     <FavoriteBorderOutlined />
